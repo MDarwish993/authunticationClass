4955d369ea6fa92ac75b1367e77e4eaa73431b07
MDarwish993 <m.darwish993@hotmail.com>
2023-09-16 12:02:22 +0000

    lab14Solution

--- src/main/java/com/mohammadDarwish/authunticationClass/controller/PostController.java
+++ src/main/java/com/mohammadDarwish/authunticationClass/controller/PostController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @Controller
@@ -23,37 +25,42 @@ public class PostController {
     SiteUserRepository siteUserRepository;
 
     @PostMapping("/add-post")
-    public RedirectView addPost(String textContent, Long siteUserId){
+    public RedirectView addPost(HttpServletRequest request,String textContent){
+        HttpSession session=request.getSession();
+
+    String username=session.getAttribute("username").toString();
+
         SiteUser siteUser=siteUserRepository
-                .findById(siteUserId)
-                .orElseThrow(()-> new RuntimeException());
+                .findByUsername(username);
 
         Post post=new Post(textContent,siteUser);
 
             postRepository.save(post);
 
-        return new RedirectView("/");
+        return new RedirectView("/postList");
     }
 
-    @GetMapping ("/posts")
-    public String getAllPosts(Model viewModel,Long siteUserId ){
-        List<Post> listOfPost=postRepository.findAll();
+    @GetMapping ("/postList")
+    public String getAllPosts(HttpServletRequest request,Model viewModel){
+        HttpSession session=request.getSession();
+        String username=session.getAttribute("username").toString();
+        viewModel.addAttribute("username",username);
 
-        viewModel.addAttribute("listOfPost",listOfPost);
-        return "/";
+        SiteUser siteUser=siteUserRepository.findByUsername(username);
+            if(siteUser !=null){
+                List<Post> listOfPost=siteUser.getPostList();
+                viewModel.addAttribute("listOfPost",listOfPost);
+            }
+
+        return "/postList";
     }
 
-
-
-
-
-
-    @DeleteMapping("/delete/{PostId}")
-    public RedirectView deletePost(@PathVariable Long PostId){
-       postRepository.deleteById(PostId);
-
-        return new RedirectView("/");
-    }
+//    @DeleteMapping("/delete/{PostId}")
+//    public RedirectView deletePost(@PathVariable Long PostId){
+//       postRepository.deleteById(PostId);
+//
+//        return new RedirectView("/postList");
+//    }
 
 
 
